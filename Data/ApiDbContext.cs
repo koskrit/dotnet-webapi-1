fa2ee578df0410acd491572f371ff385b3bd9f4d
koskrit <koskritm@gmail.com>
2024-02-02 05:09:59 +0200

    Setup Note Model in the Entity Framework Db Context

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 public class ApiDbContext : DbContext
 {
+    public virtual DbSet<Note> Notes { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseInMemoryDatabase("MyDatabase");
