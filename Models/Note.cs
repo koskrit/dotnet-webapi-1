38d2a16e845aa1c8a7da876414490459f0bfc07d
koskrit <koskritm@gmail.com>
2024-02-02 05:03:47 +0200

    Add Validations

--- Models/Note.cs
+++ Models/Note.cs
@@ -6,10 +6,17 @@ namespace Models
     public class Note
     {
         public long Id { get; set; }
+
+        [MinLength(3), MaxLength(100)]
         public string Title { get; set; }
+
+        [MinLength(3)]
         public string HtmlContent { get; set; }
+
+        [ReadOnly(true)]
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+
         public string User { get; set; }
     }
 }
