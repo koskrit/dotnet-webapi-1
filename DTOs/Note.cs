4e8b52cd52b5215b0b7279b9074f6fcd6a65ff1a
koskrit <koskritm@gmail.com>
2024-02-02 05:05:52 +0200

    Create Note DTO For the Note Controller

---
+++ DTOs/Note.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.DTOs
+{
+    public class NoteDTO
+    {
+        [MinLength(3), MaxLength(100)]
+        public string Title { get; set; }
+
+        [MinLength(3)]
+        public string HtmlContent { get; set; }
+    }
+}
