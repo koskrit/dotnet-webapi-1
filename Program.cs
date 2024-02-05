d49df4d51ffaade2a27577d4ca4d2229b510b763
koskrit <koskritm@gmail.com>
2024-02-05 08:35:22 +0200

    Setup Sentry For Error Tracking

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseSentry();
+
 builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseCors("CorsPolicy");
+app.UseSentryTracing();
 
 if (app.Environment.IsDevelopment())
 {
